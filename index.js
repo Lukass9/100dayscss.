15f96ca46b886d45f6ba4f4c2026930ac7aab0fa
Lukass9 <l.pisarek@o2.pl>
2023-07-21 08:12:07 +0200

    added static element

--- index.js
+++
@@ -1,6 +0,0 @@
-const wrapp = document.querySelector(".wrapp");
-const line = document.querySelectorAll(".no-animate");
-wrapp.addEventListener("click", () => {
-  wrapp.classList.toggle("active");
-  line.forEach((el) => el.classList.remove("no-animate"));
-});
