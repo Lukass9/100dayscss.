61d2077b3c179c2003ce59b4b288e96cc4693815
Lukass9 <l_pisarek@o2.pl>
2023-07-28 07:06:33 +0200

    finished day 7

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const search = document.querySelector(".search-icon");
 const searchInp = document.querySelector(".search-inp");
+const button = document.querySelector(".menu");
+const panel = document.querySelector(".panel");
+const dashboard = document.querySelector(".dashboard");
 
 search.addEventListener("click", () => {
+  searchInp.classList.remove("no-animation");
   searchInp.classList.toggle("visible");
-  console.log("asc");
+});
+
+button.addEventListener("click", () => {
+  panel.classList.toggle("show-dashboard");
+  dashboard.classList.toggle("slide-out-dashboard");
 });
